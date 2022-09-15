02ac699ec5c7cea546493509c7c8e46cdc2c5b3e
Bill Cook <billcook8122@gmail.com>
2022-09-15 20:06:59 +0900

    api config

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,7 @@ import { getConfig } from "@framework/api/config"
 
 export async function getStaticProps() {
   const config = getConfig()
-  console.log(config.apiUrl)
-  console.log(config.fetch)
-
-  const products = await getAllProducts()
+  const products = await getAllProducts(config)
 
   return {
     props: {
