471eb48de78ca1dcc809687cf3ab44f0619f47bb
Bill Cook <billcook8122@gmail.com>
2022-09-15 22:34:24 +0900

    tailwind and CSS variables are now configured

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,9 +1,15 @@
+import style from "./Layout.module.css"
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: Props) {
-  return<div className="layout">
-    {children}
+  return (
+  <div className={style.root}>
+    <main className="fit">
+      {children}
+    </main>
   </div>
+  )
 }
