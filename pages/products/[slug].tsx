d5a23f0e1e751db1e2a10eaad953a8b1852a2c8f
Bill Cook <billcook8122@gmail.com>
2022-09-21 10:52:49 +0900

    added basic view for information for products. KEYS ARE NEEDED LATER

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -5,6 +5,7 @@ import {
   getAllProductsPaths,
   getProduct
 } from "@framework/product"
+import { Container } from "@components/ui"
 // fetch all of the products slugs
 export const getStaticPaths: GetStaticPaths = async () => {
 
@@ -38,13 +39,52 @@ export const getStaticProps = async ({
 export default function ProductSlug({
   product }: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  console.log(JSON.stringify(product, null, 2))
+
   return (
-    <div>
-      {JSON.stringify(product, null, 2)}
-    </div>
+    <Container>
+      <p>id: {product?.id}</p>
+      <p>name: {product?.name}</p>
+      <p>price value: {product?.price.value}</p>
+      <p>price currency: {product?.price.currencyCode}</p>
+      <p>description: {product?.description}</p>
+
+      <h1 className="mb-4">OPTIONS</h1>
+      <div>
+        { product?.options.map(option =>
+          <div key="">
+            <p>Name: {option.displayName}</p>
+            { option.values.map(value =>
+              <div key="">
+                <p>Label: {value.label}</p>
+                <p>Hex Color: {value.hexColor}</p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <h1 className="mb-4">VARIANTS</h1>
+      <div>
+        { product?.variants.map(variant =>
+          <div key="">
+            <p>Variant Name: {variant.name}</p>
+            { variant.options.map(vo =>
+              <div key="">
+                <p>Name: {vo.displayName}</p>
+                { vo.values.map(value =>
+                  <div key="">
+                    <p>Label: {value.label}</p>
+                    <p>Hexcolot: {value.hexColor}</p>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </Container>
   )
 }
 
-
 ProductSlug.Layout = Layout
+
+// NOTE: make sure to assign a key for the iterations so that DOM manipulation can be applied.
