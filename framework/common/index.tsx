192c645591e6e0949f47bab52fcfa0c26108ac34
Bill Cook <billcook8122@gmail.com>
2022-09-22 10:02:20 +0900

    basic configuration

--- framework/common/index.tsx
+++ framework/common/index.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useMemo } from "react";
 import { ApiConfig } from "./types/api"
 
 interface ApiProviderProps {
   children: ReactNode | ReactNode
-  config: ApiConfig | {testKey: string}
+  config: ApiConfig
 }
 
 
@@ -11,8 +11,14 @@ export const ApiContext = createContext({})
 
 export const ApiProvider = ({children, config}: ApiProviderProps) => {
 
+  const coreConfig = useMemo(() => {
+    return {
+      fetcher: config.fetch
+    }
+  }, [config.fetch])
+
   return (
-    <ApiContext.Provider value={config}>
+     <ApiContext.Provider value={coreConfig}>
       {children}
     </ApiContext.Provider>
   )
