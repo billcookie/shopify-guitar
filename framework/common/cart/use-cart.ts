6e326b42d5132e2b5f35aa46840d799b05dadf91
Bill Cook <billcook8122@gmail.com>
2022-09-22 15:14:47 +0900

    cookie id

--- framework/common/cart/use-cart.ts
+++ framework/common/cart/use-cart.ts
@@ -1,5 +1,7 @@
 import { ApiHooks } from "@common/types/hooks"
 import { useHook, useSWRHook } from "../utils/use-hook"
+import Cookies from "js-cookie"
+import { SHOPIFY_CHECKOUT_ID_COOKIE } from "@framework/const"
 
 
 
@@ -7,7 +9,14 @@ import { useHook, useSWRHook } from "../utils/use-hook"
 const useCart = () => {
 
   const hook = useHook((hooks: ApiHooks) => hooks.cart.useCart)
-  return useSWRHook({...hook})
+  const fetcherWrapper: typeof hook.fetcher = (context) => {
+    context.input.checkoutId = Cookies.get(SHOPIFY_CHECKOUT_ID_COOKIE)
+    debugger
+    return hook.fetcher(context)
+  }
+
+
+  return useSWRHook({...hook, fetcher: fetcherWrapper})
 }
 
 export default useCart
