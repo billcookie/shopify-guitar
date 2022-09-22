22f89c2326dd4612828a7e265bb4e67aa7427dbf
Bill Cook <billcook8122@gmail.com>
2022-09-22 09:59:20 +0900

    added basic api

--- framework/common/index.tsx
+++ framework/common/index.tsx
@@ -1,15 +1,15 @@
 import { createContext, ReactNode, useContext } from "react";
-import { getConfig } from "@framework/api/config"
+import { ApiConfig } from "./types/api"
 
 interface ApiProviderProps {
   children: ReactNode | ReactNode
+  config: ApiConfig | {testKey: string}
 }
 
-const config = getConfig()
 
 export const ApiContext = createContext({})
 
-export function ApiProvider ({children}: ApiProviderProps) {
+export const ApiProvider = ({children, config}: ApiProviderProps) => {
 
   return (
     <ApiContext.Provider value={config}>
